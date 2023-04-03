5865db3ca665b353ad4c56d9a4f374314147b1c2
michaelprys <musha09cz@gmail.com>
2023-04-04 00:41:34 +0200

    routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex p-8 justify-center">
+  <div>
     <input
       type="text"
       class="rounded border-2 border-gray-200 w-full"
@@ -18,14 +18,16 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import axiosClient from "../axiosClient.js";
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
 
 onMounted(async () => {
   const response = await axiosClient.get("/list.php?i=list");
   console.log(response.data);
+  ingredients.value = response.data;
 });
 </script>
